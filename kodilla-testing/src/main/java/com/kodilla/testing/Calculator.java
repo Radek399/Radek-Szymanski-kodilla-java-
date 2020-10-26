0e3a3338185ab1977406863a5594c12126ac7804
Radek Szymanski <r.szymanski399@gmail.com>
2020-10-26 11:45:54 +0100

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator.java
@@ -1,14 +1,12 @@
-package com.kodilla.testing.calculator;
+package com.kodilla.testing;
 
 public class Calculator {
 
     public int add(int a, int b) {
         return a + b;
     }
-
     public int subtract(int a, int b) {
         return a - b;
     }
 }
-
 
